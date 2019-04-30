65b5d5b91ad1aebdc3cf987148c77b370a5ed409
Thomas Fabritius <tfabritius@gmail.com>
2019-04-30 13:18:03 +0200

    Add error page

---
+++ layouts/error.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <v-alert :value="true" type="error" outline>
+      {{ error.message }}
+    </v-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line
+  props: { error: Object }
+}
+</script>
