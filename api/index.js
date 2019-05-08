15ba721b8ae402613cc0c92785cfeefa8cdcfbf6
Thomas Fabritius <tfabritius@gmail.com>
2019-05-08 07:54:21 +0200

    Improve error handling in api

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import Debug from 'debug'
 import contact from './contact.js'
 import currencies from './currencies.js'
 import securities from './securities.js'
 import stats from './stats.js'
+const log = Debug('api:index')
 
 const app = express()
 
@@ -30,9 +32,33 @@ app.use('/currencies', currencies)
 app.use('/securities', securities)
 app.use('/stats', stats)
 
-// This has to be the last route!
-app.route('*').get(function(req, res) {
-  res.status(404).json({ message: 'Page not found.' })
+/**
+ * Return 404 if nothing has matched so far.
+ */
+app.use(function(req, res, next) {
+  const err = new Error('Page not found')
+  err.statusCode = 404
+  next(err)
+})
+
+/**
+ * Generic error handler
+ */
+app.use(function(err, req, res, next) {
+  if (!err.statusCode) err.statusCode = 500
+
+  if (err.statusCode >= 500) {
+    log(err.stack)
+  }
+
+  // Be verbose in development mode
+  if (process.env.NODE_ENV !== 'production') {
+    return res
+      .status(err.statusCode)
+      .json({ message: err.message, stack: err.stack })
+  }
+
+  res.status(err.statusCode).json({ message: err.message })
 })
 
 export default {
