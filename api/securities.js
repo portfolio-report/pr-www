133270ad993b5d1a9e9fb225b4d592786ae1be4b
Thomas Fabritius <tfabritius@gmail.com>
2019-05-13 20:55:23 +0200

    Use nedb-promises for securities

--- api/securities.js
+++ api/securities.js
@@ -1,7 +1,7 @@
 import express from 'express'
-import NeDB from 'nedb'
+import NeDB from 'nedb-promises'
 
-const db = new NeDB({
+const db = NeDB.create({
   filename: './db/securities.db.json',
   autoload: true
 })
@@ -12,28 +12,24 @@ router.get('/', function(req, res) {
   res.json({ status: 'ok' })
 })
 
-router.route('/:uuid').get(function(req, res) {
+router.route('/:uuid').get(async function(req, res) {
   const uuid = req.params.uuid
 
-  db.findOne({ uuid: uuid }, function(err, security) {
-    if (err) {
-      res.send(err)
-      return
-    }
-
-    if (!security) {
-      res.status(404).json({ message: 'Security not found.' })
-      return
-    }
-
-    // Hide internal ID
-    security._id = undefined
-
-    res.json(security)
+  const security = await db.findOne({
+    uuid: uuid
   })
+  if (!security) {
+    res.status(404).json({ message: 'Security not found.' })
+    return
+  }
+
+  // Hide internal ID
+  security._id = undefined
+
+  res.json(security)
 })
 
-router.route('/search/:search').get(function(req, res) {
+router.route('/search/:search').get(async function(req, res) {
   const search = req.params.search
   const paramType = req.query.type
 
@@ -42,35 +38,30 @@ router.route('/search/:search').get(function(req, res) {
   const regexStartIMatch = new RegExp('^' + search, 'i')
   const regexExactIMatch = new RegExp('^' + search + '$', 'i')
 
-  db.find({
-    $and: [
-      {
-        $or: [
-          { name: { $regex: regexStartIMatch } },
-          { isin: { $regex: regexExactIMatch } },
-          { wkn: { $regex: regexExactIMatch } },
-          { 'markets.XFRA.symbol': { $regex: regexExactIMatch } },
-          { 'markets.XNAS.symbol': { $regex: regexExactIMatch } },
-          { 'markets.XNYS.symbol': { $regex: regexExactIMatch } }
-        ]
-      },
-      typeCondition
-    ]
-  })
-    .limit(10)
-    .exec(function(err, docs) {
-      if (err) {
-        res.send(err)
-        return
-      }
-
-      // Hide internal IDs
-      docs.forEach(doc => {
-        doc._id = undefined
-      })
-
-      res.json(docs)
+  const docs = await db
+    .find({
+      $and: [
+        {
+          $or: [
+            { name: { $regex: regexStartIMatch } },
+            { isin: { $regex: regexExactIMatch } },
+            { wkn: { $regex: regexExactIMatch } },
+            { 'markets.XFRA.symbol': { $regex: regexExactIMatch } },
+            { 'markets.XNAS.symbol': { $regex: regexExactIMatch } },
+            { 'markets.XNYS.symbol': { $regex: regexExactIMatch } }
+          ]
+        },
+        typeCondition
+      ]
     })
+    .limit(10)
+
+  // Hide internal IDs
+  docs.forEach(doc => {
+    doc._id = undefined
+  })
+
+  res.json(docs)
 })
 
 export default router
