d02979d0c6f7442ae2244dc8cc489efa0f04cd63
Thomas Fabritius <tfabritius@gmail.com>
2019-05-14 12:31:03 +0200

    Support mdi (material design icons)

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,7 @@ module.exports = {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['@/plugins/vuetify'],
+  plugins: ['@/plugins/vuetify', '@/plugins/materialdesignicons'],
 
   /*
    ** Nuxt.js modules
