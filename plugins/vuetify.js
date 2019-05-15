bff990d3bc847654c8fdf244d1644fe5a0c52ba9
Thomas Fabritius <tfabritius@gmail.com>
2019-05-15 17:58:33 +0200

    Change color scheme

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -4,12 +4,12 @@ import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
   theme: {
-    primary: colors.blue.darken2,
+    primary: '#006e90', // colors.blue.darken2
     accent: colors.grey.darken3,
-    secondary: colors.amber.darken3,
-    info: colors.teal.lighten1,
-    warning: colors.amber.base,
+    secondary: '#f18f01', // colors.amber.darken3
+    info: '#006e90', // colors.teal.lighten1
+    warning: '#f18f01', // colors.amber.base
     error: colors.deepOrange.accent4,
-    success: colors.green.accent3
+    success: '#99C24D' // colors.green.accent3
   }
 })
