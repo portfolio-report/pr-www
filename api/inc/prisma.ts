caaa3daedddf0b6599ebfa647dd1f8639878c66b
Thomas Fabritius <tfabritius@gmail.com>
2020-01-27 21:51:29 +0100

    feat: allow logging of prisma queries

--- api/inc/prisma.ts
+++ api/inc/prisma.ts
@@ -1,3 +1,12 @@
 import { PrismaClient } from '@prisma/client'
+import Debug from 'debug'
 
-export const prisma = new PrismaClient()
+const log = Debug('pr-www:prisma')
+
+export const prisma = new PrismaClient({
+  log: [{ emit: 'event', level: 'query' }],
+})
+
+prisma.$on('query', (e) => {
+  log(`SQL: ${e.query}, params: ${e.params}, duration: ${e.duration}`)
+})
