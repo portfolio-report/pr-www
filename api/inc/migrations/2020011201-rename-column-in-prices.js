bef2a23f10d0bcdd0da073308574a810ff615512
Thomas Fabritius <tfabritius@gmail.com>
2020-02-02 23:14:43 +0100

    Retain constraint when renaming column (sequelize's renameColumn drops it)

--- api/inc/migrations/2020011201-rename-column-in-prices.js
+++ api/inc/migrations/2020011201-rename-column-in-prices.js
@@ -1,11 +1,47 @@
 module.exports = {
-  up: async (queryInterface, _Sequelize) => {
+  up: async (queryInterface, Sequelize) => {
     const transaction = await queryInterface.sequelize.transaction()
     try {
-      await queryInterface.renameColumn('prices', 'value', 'close', {
-        transaction,
-      })
+      // Rename column 'value' to 'close'
+      await queryInterface.createTable(
+        'prices_new',
+        {
+          id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+          },
+          marketId: {
+            type: Sequelize.INTEGER,
+            references: {
+              model: 'markets',
+              key: 'id',
+            },
+            onUpdate: 'cascade',
+            onDelete: 'cascade',
+          },
+          date: {
+            type: Sequelize.DATEONLY,
+            allowNull: false,
+          },
+          close: {
+            type: Sequelize.DECIMAL(10, 4),
+            allowNull: false,
+          },
+        },
+        {
+          transaction,
+        }
+      )
 
+      await queryInterface.sequelize.query(
+        `INSERT INTO prices_new SELECT id, marketId, date, value as close FROM prices`,
+        { transaction }
+      )
+
+      await queryInterface.dropTable('prices', { transaction })
+      await queryInterface.renameTable('prices_new', 'prices', { transaction })
+
+      // Restore indices
       for (const indexOptions of [
         { fields: ['marketId'] },
         { fields: ['marketId', 'date'], unique: true },
@@ -23,13 +59,49 @@ module.exports = {
     }
   },
 
-  down: async (queryInterface, _Sequelize) => {
+  down: async (queryInterface, Sequelize) => {
     const transaction = await queryInterface.sequelize.transaction()
     try {
-      await queryInterface.renameColumn('prices', 'close', 'value', {
-        transaction,
-      })
+      // Rename column 'close' to 'value'
+      await queryInterface.createTable(
+        'prices_new',
+        {
+          id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+          },
+          marketId: {
+            type: Sequelize.INTEGER,
+            references: {
+              model: 'markets',
+              key: 'id',
+            },
+            onUpdate: 'cascade',
+            onDelete: 'cascade',
+          },
+          date: {
+            type: Sequelize.DATEONLY,
+            allowNull: false,
+          },
+          value: {
+            type: Sequelize.DECIMAL(10, 4),
+            allowNull: false,
+          },
+        },
+        {
+          transaction,
+        }
+      )
 
+      await queryInterface.sequelize.query(
+        `INSERT INTO prices_new SELECT id, marketId, date, close as value FROM prices`,
+        { transaction }
+      )
+
+      await queryInterface.dropTable('prices', { transaction })
+      await queryInterface.renameTable('prices_new', 'prices', { transaction })
+
+      // Restore indices
       for (const indexOptions of [
         { fields: ['marketId'] },
         { fields: ['marketId', 'date'], unique: true },
