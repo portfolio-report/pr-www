33902b9be0c29a76a25e8c8303770b20c237297e
Thomas Fabritius <tfabritius@gmail.com>
2020-05-08 20:35:45 +0200

    Add events to db model

--- api/inc/sequelize.ts
+++ api/inc/sequelize.ts
@@ -21,6 +21,7 @@ export class Security extends Model {
   public staged!: boolean
 
   public readonly markets?: Array<Market>
+  public readonly events?: Array<Event>
 }
 
 Security.init(
@@ -140,6 +141,52 @@ export class ClientUpdate extends Model {
   public country!: string | null
   public useragent!: string | null
 }
+
+export class Event extends Model {
+  public id!: number
+  public date!: Date
+  public type!: string
+
+  public amount!: number | null
+  public currencyCode!: string | null
+  public ratio!: string | null
+
+  public securityId!: number
+}
+
+Event.init(
+  {
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    type: {
+      type: DataTypes.STRING(10),
+      allowNull: false,
+    },
+    amount: {
+      type: DataTypes.DECIMAL(10, 4),
+      allowNull: true,
+    },
+    currencyCode: {
+      type: DataTypes.STRING(3),
+      allowNull: true,
+    },
+    ratio: {
+      type: DataTypes.STRING(10),
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'event',
+    timestamps: false,
+  }
+)
+
+// Implicitely add securityId
+Event.belongsTo(Security, { onDelete: 'cascade' })
+Security.hasMany(Event, { onDelete: 'cascade' })
 
 ClientUpdate.init(
   {
