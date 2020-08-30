c6730fb2956426445250520e37ca81d47db8f41d
Thomas Fabritius <tfabritius@gmail.com>
2020-08-30 13:20:20 +0200

    Add footer to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -20,6 +20,43 @@
         <nuxt />
       </v-container>
     </v-content>
+    <v-footer>
+      <v-spacer />
+      <v-tooltip top>
+        <template v-slot:activator="{ on }">
+          <v-btn
+            href="https://forum.portfolio-performance.info/"
+            icon
+            v-on="on"
+          >
+            <v-icon>{{ mdiForum }}</v-icon>
+          </v-btn>
+        </template>
+        <span>Get help and discuss</span>
+      </v-tooltip>
+      <v-tooltip top>
+        <template v-slot:activator="{ on }">
+          <v-btn to="/contact" icon v-on="on">
+            <v-icon>{{ mdiEmail }}</v-icon>
+          </v-btn>
+        </template>
+        <span>Get in contact</span>
+      </v-tooltip>
+      <v-tooltip top>
+        <template v-slot:activator="{ on }">
+          <v-btn
+            href="https://www.github.com/tfabritius/pr-www"
+            target="_blank"
+            icon
+            v-on="on"
+          >
+            <v-icon>{{ mdiSourceRepository }}</v-icon>
+          </v-btn>
+        </template>
+        <span>Source code</span>
+      </v-tooltip>
+      <v-spacer></v-spacer>
+    </v-footer>
   </v-app>
 </template>
 
@@ -32,9 +69,14 @@
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
+import { mdiEmail, mdiForum, mdiSourceRepository } from '@mdi/js'
 
 @Component
 export default class DefaultLayout extends Vue {
   title = 'Portfolio Report'
+
+  mdiForum = mdiForum
+  mdiEmail = mdiEmail
+  mdiSourceRepository = mdiSourceRepository
 }
 </script>
--- pages/search.vue
+++ pages/search.vue
@@ -26,40 +26,6 @@
             </v-alert>
           </v-card-text>
           <v-card-actions>
-            <v-tooltip top>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  href="https://forum.portfolio-performance.info/"
-                  icon
-                  v-on="on"
-                >
-                  <v-icon>{{ mdiForum }}</v-icon>
-                </v-btn>
-              </template>
-              <span>Get help and discuss</span>
-            </v-tooltip>
-            <v-tooltip top>
-              <template v-slot:activator="{ on }">
-                <v-btn to="/contact" icon v-on="on">
-                  <v-icon>{{ mdiEmail }}</v-icon>
-                </v-btn>
-              </template>
-              <span>Get in contact</span>
-            </v-tooltip>
-            <v-tooltip top>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  href="https://www.github.com/tfabritius/pr-www"
-                  target="_blank"
-                  icon
-                  v-on="on"
-                >
-                  <v-icon>{{ mdiSourceRepository }}</v-icon>
-                </v-btn>
-              </template>
-              <span>Source code</span>
-            </v-tooltip>
-
             <v-spacer />
             <v-btn
               type="submit"
@@ -117,21 +83,12 @@
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
-import {
-  mdiChartLine,
-  mdiDragVariant,
-  mdiEmail,
-  mdiForum,
-  mdiSourceRepository,
-} from '@mdi/js'
+import { mdiChartLine, mdiDragVariant } from '@mdi/js'
 
 @Component
 export default class SearchPage extends Vue {
-  mdiForum = mdiForum
-  mdiEmail = mdiEmail
   mdiChartLine = mdiChartLine
   mdiDragVariant = mdiDragVariant
-  mdiSourceRepository = mdiSourceRepository
 
   searchFormValid = false
   searchTerm = ''
