5b88e9e898e43fdf5cc553272535e5815df4a1d7
Thomas Fabritius <tfabritius@gmail.com>
2020-09-02 10:10:53 +0200

    Show events in UI

--- pages/securities/_uuid.vue
+++ pages/securities/_uuid.vue
@@ -1,33 +1,7 @@
 <template>
   <v-row align="center" justify="center">
     <v-col cols="12" sm="8" md="6">
-      <span class="headline">{{ security.name }}</span>
-      <ul>
-        <li>
-          ISIN: <b>{{ security.isin }}</b>
-        </li>
-        <li>
-          WKN: <b>{{ security.wkn }}</b>
-        </li>
-        <li v-if="security.symbolXfra">
-          Symbol (Frankfurt):
-          <b>{{ security.symbolXfra }}</b>
-        </li>
-        <li v-if="security.symbolXnas">
-          Symbol (NASDAQ):
-          <b>{{ security.symbolXnas }}</b>
-        </li>
-        <li v-if="security.symbolXnys">
-          Symbol (New York):
-          <b>{{ security.symbolXnys }}</b>
-        </li>
-        <li>
-          Type:
-          <v-chip small color="primary" text-color="white">
-            {{ security.securityType }}
-          </v-chip>
-        </li>
-      </ul>
+      <div class="headline">{{ security.name }}</div>
 
       <v-tooltip right>
         <template v-slot:activator="{ on }">
@@ -51,24 +25,97 @@
         </div>
       </v-tooltip>
 
-      <div v-for="market in markets" :key="market.marketCode" class="mt-4">
-        <span class="title">{{ market.name }}</span>
-        <ul>
-          <li>
-            Code: <b>{{ market.marketCode }}</b>
-          </li>
-          <li>
-            Symbol: <b>{{ market.symbol }}</b>
-          </li>
-          <li>
-            Currency: <b>{{ market.currencyCode || '-' }}</b>
-          </li>
-          <li>
-            Prices available: <b>{{ market.firstPriceDate }}</b> -
-            <b>{{ market.lastPriceDate }}</b>
-          </li>
-        </ul>
-      </div>
+      <v-tabs grow>
+        <v-tab key="overview">Overview</v-tab>
+        <v-tab key="prices">Prices</v-tab>
+        <v-tab key="events">Events</v-tab>
+
+        <v-tab-item key="overview">
+          <ul>
+            <li>
+              ISIN: <b>{{ security.isin }}</b>
+            </li>
+            <li>
+              WKN: <b>{{ security.wkn }}</b>
+            </li>
+            <li v-if="security.symbolXfra">
+              Symbol (Frankfurt):
+              <b>{{ security.symbolXfra }}</b>
+            </li>
+            <li v-if="security.symbolXnas">
+              Symbol (NASDAQ):
+              <b>{{ security.symbolXnas }}</b>
+            </li>
+            <li v-if="security.symbolXnys">
+              Symbol (New York):
+              <b>{{ security.symbolXnys }}</b>
+            </li>
+            <li>
+              Type:
+              <v-chip small color="primary" text-color="white">
+                {{ security.securityType }}
+              </v-chip>
+            </li>
+          </ul>
+        </v-tab-item>
+
+        <v-tab-item key="prices">
+          <div v-for="market in markets" :key="market.marketCode" class="mt-4">
+            <span class="title">{{ market.name }}</span>
+            <ul>
+              <li>
+                Code: <b>{{ market.marketCode }}</b>
+              </li>
+              <li>
+                Symbol: <b>{{ market.symbol }}</b>
+              </li>
+              <li>
+                Currency: <b>{{ market.currencyCode || '-' }}</b>
+              </li>
+              <li>
+                Prices available: <b>{{ market.firstPriceDate }}</b> -
+                <b>{{ market.lastPriceDate }}</b>
+              </li>
+            </ul>
+          </div>
+        </v-tab-item>
+
+        <v-tab-item key="events">
+          <v-simple-table>
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th class="text-left">Date</th>
+                  <th class="text-left">Type</th>
+                  <th class="text-left">...</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="event in security.events"
+                  :key="event.date + event.type"
+                >
+                  <td>{{ event.date }}</td>
+                  <td style="text-transform: capitalize">{{ event.type }}</td>
+                  <td>
+                    {{
+                      event.type === 'dividend'
+                        ? event.amount + ' ' + event.currencyCode
+                        : ''
+                    }}
+                    {{ event.type === 'split' ? event.ratio : '' }}
+                  </td>
+                </tr>
+                <tr v-if="security.events.length === 0">
+                  <td colspan="3">No events available</td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+        </v-tab-item>
+      </v-tabs>
+
+      <v-tabs-items> </v-tabs-items>
     </v-col>
   </v-row>
 </template>
