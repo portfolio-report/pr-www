b9c107e327699e2930bec4b106bd2125f1432683
Thomas Fabritius <tfabritius@gmail.com>
2021-01-19 20:46:47 +0100

    build: remove migration tooling

--- api/inc/migrations/sqlite2postgres/migrate.sh
+++
@@ -1,8 +0,0 @@
-# Expects DATABASE_URL="postgresql://username:password@host:port/database"
-
-# Disable exit on non 0
-set +e
-
-pgloader --with "include drop" --with "create tables" --with "create no indexes" --with "quote identifiers" 'sqlite://../../../../db/database.sqlite' "$DATABASE_URL"
-
-psql "$DATABASE_URL" < afterpgloader.sql
