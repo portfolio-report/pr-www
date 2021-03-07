db730da6b09f9d733b4b47759adbbcfba457062a
Thomas Fabritius <tfabritius@gmail.com>
2021-03-07 13:04:18 +0100

    feat: polish 404 page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <v-alert :value="true" type="error" outlined>
+    <div v-if="error.statusCode === 404">
+      <h1>Page not found 😟</h1>
+      <p>
+        Sorry, this page does not exist. If you think, this is wrong, please
+        <NuxtLink to="/contact">let us know</NuxtLink>.
+      </p>
+      <NuxtLink to="/">Go to home page</NuxtLink>
+    </div>
+    <v-alert v-else :value="true" type="error" outlined>
       {{ error.message }}
     </v-alert>
   </div>
