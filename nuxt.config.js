78994ff48bd9f5104cb60e1c3657958f1b00b5fb
Thomas Fabritius <tfabritius@gmail.com>
2021-03-31 21:46:04 +0200

    feat: change default title

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: pkg.name,
+    title: 'Portfolio Report',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
