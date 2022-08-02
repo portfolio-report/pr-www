b6d1111bb1437155288fa52618b04686c11ebe3c
Thomas Fabritius <tfabritius@gmail.com>
2022-08-02 22:32:34 +0200

    Remove validator

--- pages/contact.vue
+++ pages/contact.vue
@@ -70,8 +70,17 @@
 
 <script lang="ts">
 import { defineComponent, ref, useContext } from '@nuxtjs/composition-api'
-import isEmail from 'validator/lib/isEmail'
 import BtnLoading from '../components/BtnLoading.vue'
+
+function isEmail(str: string): boolean {
+  if (typeof str !== 'string') {
+    return false
+  }
+
+  const regex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return regex.test(str.toLowerCase())
+}
 
 export default defineComponent({
   name: 'ContactPage',
