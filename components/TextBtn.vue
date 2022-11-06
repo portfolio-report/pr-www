5c04da23357a56214f33b94a306a25d47a7fa3e2
Thomas Fabritius <tfabritius@gmail.com>
2022-11-06 19:33:17 +0100

    Fix missing component

---
+++ components/TextBtn.vue
@@ -0,0 +1,48 @@
+<template>
+  <Button
+    :type="type"
+    :icon="icons"
+    :class="{
+      'p-button-text': !solid,
+      'p-button-rounded': !label,
+      'x-button-small': small,
+      'p-button-secondary': color == 'secondary',
+      'p-button-success': color == 'success',
+    }"
+    :label="label"
+    :form="form"
+    @click="emit('click', $event)"
+  >
+    <slot />
+  </Button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  color?: 'secondary' | 'success'
+  icon?: string
+  label?: string
+  small?: boolean
+  solid?: boolean
+  type?: 'button' | 'submit' | 'reset'
+  form?: string
+}>()
+
+const emit = defineEmits<{
+  (e: 'click', arg1: MouseEvent): void
+}>()
+
+const icons = computed(() => {
+  if (props.icon && !props.small) {
+    return props.icon + ' i-lg'
+  }
+  return props.icon
+})
+</script>
+
+<style scoped>
+.p-button.x-button-small {
+  height: 2rem !important;
+  width: 2rem !important;
+}
+</style>
