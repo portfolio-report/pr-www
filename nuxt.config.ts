2629458ef15b1ecd924ee46183d087d13fafdf75
Thomas Fabritius <tfabritius@gmail.com>
2024-09-11 12:55:31 +0200

    Switch font family

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
   css: [
     '~/assets/styles/main.scss',
     '@fontsource/fira-mono',
-    '@fontsource-variable/inter',
+    '@fontsource/fira-sans',
   ],
 
   modules: [
